729976e4bef804991f61554332337051329c0955
Scott Carroll <scott.carroll@gmail.com>
2021-06-07 08:00:11 -0700

    Add Feature not implemented at this level

---
+++ src/net/kensaigm/dp/CompositeMenuDemo.java
@@ -0,0 +1,21 @@
+package net.kensaigm.dp;
+
+public class CompositeMenuDemo {
+    public static void main(String[] args) {
+        Menu mainMenu = new Menu("File", "/file");
+
+        MenuItem safetyMenuItem = new MenuItem("Safety", "/safety");
+
+        mainMenu.add(safetyMenuItem);
+
+        Menu claimsSubMenu = new Menu("Claims", "/claims");
+
+        mainMenu.add(claimsSubMenu);
+
+        MenuItem personalClaimsMenu = new MenuItem("Personal Claim", "/personalClaims");
+
+        claimsSubMenu.add(personalClaimsMenu);
+
+        System.out.println(mainMenu.toString());
+    }
+}
---
+++ src/net/kensaigm/dp/MenuItem.java
@@ -0,0 +1,13 @@
+package net.kensaigm.dp;
+
+public class MenuItem extends MenuComponent {
+    public MenuItem(String name, String url){
+        this.name = name;
+        this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return print(this);
+    }
+}
